ff494fc84bc2abfda347fbbac68deee55c5be064
João Eduardo Dias <diasje@gmail.com>
2013-11-25 14:52:57 +0000

    Update con.php

--- con.php
+++ con.php
@@ -10,19 +10,13 @@ $db = "";
 // Specify MySql Table
 $table = "";
 //Specify LDAP Server
-$host = "ldap://bracnh.server.NET";
+$host = "ldap://branch.server.net";
 //Specify User
 $user = "domain\username";
 // Specify Password
 $pswd = "somepass";
 // DN to search - you can use LdapAdmin.org to fetch this data
-$dn = "OU=User,OU=Accounts,OU=PT1,DC=heiway,DC=net";
+$dn = "OU=User,OU=Accounts,OU=PT1,DC=server,DC=net";
 // Specify only those parameters we're interested in displaying LdapAdmin.org to fetch this data
 $attrs = array("department","displayName","mail","name","employeeid","l","useraccountcontrol","lastlogon");
-// Connect to MySql Database
-$con = mysql_connect("$sqlhost","$sqluser","$sqlpass"); 
-    if (!$con) 
-    { 
-    die('Could not connect: ' . mysql_error()); 
-    } 
 ?>

ea89c9819725409ff39922753fef9e04ab1bd14d
João Eduardo Dias <diasje@gmail.com>
2013-11-25 14:12:49 +0000

    Create search_users.php

---
+++ search_users.php
@@ -0,0 +1,57 @@
+<?php
+// Designate a few variables
+error_reporting(0);
+// Fetch variables file
+include 'con.php';
+// Insert into MySql DB (Optional) Uncomment the line below
+// include 'sql.php';
+//Connect to LDAP server
+$ad = ldap_connect($host)
+      or die( "Could not connect!" );
+
+// Set version number
+ldap_set_option($ad, LDAP_OPT_PROTOCOL_VERSION, 3)
+     or die ("Could not set ldap protocol");
+
+// Binding to LDAP server
+$bd = ldap_bind($ad, $user, $pswd)
+      or die ("Could not bind");
+
+// Create the filter from the search parameters
+$filter = $_POST['filter']."=".$_POST['keyword']."*";
+
+$search = ldap_search($ad, $dn, $filter, $attrs)
+          or die ("ldap search failed");
+
+$entries = ldap_get_entries($ad, $search);
+
+if ($entries["count"] > 0) {
+  echo "<table border='1' width='100%'>";
+  echo "<tr>";
+    echo "<td>State:</td>";
+  echo "<td>Departament:</td>";
+  echo "<td>Name:</td>";
+  echo "<td>User:</td>";
+  echo "<td>Employe:</td>";
+  echo "<td>Email:</td>";
+	echo "<td>Local:</td>";
+  echo "</tr>";
+  
+for ($i=0; $i<$entries["count"]; $i++) {
+  echo "<tr>";
+   echo "<td>".$entries[$i]["useraccountcontrol"][0]."</td>";
+   echo "<td>".$entries[$i]["department"][0]."</td>";
+  echo iconv("UTF-8", "ISO-8859-1","<td>".$entries[$i]["displayname"][0]."</td>");
+    echo "<td>".$entries[$i]["name"][0]."</td>";
+  echo "<td>".$entries[$i]["employeeid"][0]."</td>";
+  echo "<td> <a href='mailto:".$entries[$i]["mail"][0]."'>".$entries[$i]["mail"][0]."</a></td>";
+   echo "<td>".$entries[$i]["l"][0]."</td>";
+  echo "</tr>";
+}
+  echo "</table>";
+} else {
+   echo iconv("UTF-8", "ISO-8859-1","<p>Ops não encontrámos nada!</p>");
+}
+
+ldap_unbind($ad);
+?>

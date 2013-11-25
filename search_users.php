7b2aac131d1ecca0496e0327ee07bc629ccd235d
João Eduardo Dias <diasje@gmail.com>
2013-11-25 14:21:35 +0000

    Update search_users.php

--- search_users.php
+++ search_users.php
@@ -1,4 +1,11 @@
 <?php
+/* ----- PHP LDAP QUERY ----- */
+// With this script you can query a certain ldap server and fetch a few user data
+// there is an option to save the query to database, just fallow the instructions in the readme file
+// if you wold like some support, please refer to http://diasje.com
+/* ----- PHP LDAP QUERY ----- */
+
+
 // Designate a few variables
 error_reporting(0);
 // Fetch variables file
@@ -24,9 +31,45 @@ $search = ldap_search($ad, $dn, $filter, $attrs)
 
 $entries = ldap_get_entries($ad, $search);
 
+/* ----- START DB INSERT ------ */
 // Insert into MySql DB (Optional) Uncomment the line below
-// include 'sql.php';
-
+// $sel = mysql_select_db($db, $con); 
+//     if (!$sel) 
+//     { 
+//     die('Could not select DB: ' . mysql_error()); 
+//     } 
+// 
+//     for ($i=0; $i<$entries["count"]; $i++) 
+//     { 
+// 
+//     $uac = $entries[$i]["useraccountcontrol"][0]; 
+//     $dep = $entries[$i]["department"][0]; 
+// 	$disn = iconv("UTF-8", "ISO-8859-1", $entries[$i]["displayname"][0]);
+// 	$name = $entries[$i]["name"][0]; 
+// 	$empid = $entries[$i]["employeeid"][0]; 
+// 	$mail = $entries[$i]["mail"][0]; 
+// 	$l = $entries[$i]["l"][0]; 
+// 	$logon = $entries[$i]["lastlogon"][0];
+//          
+//             mysql_query("
+// 			INSERT INTO 
+// 				$table (State, Departament, Name, User, Employe, Email, Local, lastlogn) 
+//             VALUES 
+// 				('$uac', '$dep', '$disn', '$name', '$empid', '$mail', '$l', '$logon')
+// 			ON DUPLICATE KEY UPDATE			
+// 			Estado='$uac',
+// 			Departamento='$dep',
+// 			Nome='$disn',
+// 			User='$name',
+// 			Ne='$empid',
+// 			Email='$mail',
+// 			Local='$l',
+// 			lastlogh='$logon'
+// 		 ");
+// 
+//     } 
+//   mysql_close($con); 
+/* ----- END DB INSERT ------ */
 
 if ($entries["count"] > 0) {
   echo "<table border='1' width='100%'>";
